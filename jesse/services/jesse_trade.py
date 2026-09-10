79e2618cd1a858fc7808a50e324ff451218c4d4a
Saleh Mir <liveproduction@special.lu>
2026-09-10 18:50:58 +0200

    fix: restore exchange-log attachments; release 3.1.3

--- jesse/services/jesse_trade.py
+++ jesse/services/jesse_trade.py
@@ -37,9 +37,9 @@ def report_exception(
             path_log = f'storage/logs/backtest-mode/{session_id}.txt'
         elif mode == 'live':
             path_log = f'storage/logs/live-mode/{session_id}.txt'
+            path_exchange_log = f'storage/logs/live-mode/{session_id}-raw-exchange-logs.txt'
         elif mode == 'significance-test':
             path_log = f'storage/logs/significance-test-mode/{session_id}.txt'
-            path_exchange_log = f'storage/logs/live-mode/{session_id}-raw-exchange-logs.txt'
         else:
             raise ValueError('Invalid mode')
 
